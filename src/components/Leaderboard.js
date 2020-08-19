52a785d09738babcbd6903c94481f5acc816be3b
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-08-19 16:38:02 +0100

    refactor: sorting of users to display

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,25 +1,25 @@
-import React, { Component } from  'react'
+import React from  'react'
 import { connect } from 'react-redux'
 
-class LeaderBoard extends Component {
-    render() {
-        const { usersList } = this.props
+const LeaderBoard = (props) => {
+    const { usersList } = props
 
-        return(
-            <div>
-                <ul>
-                    {Object.keys(usersList).map((user) => (
-                        <li key={user}>{user}</li>
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <ul>
+                {usersList.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 const mapStateToProps = ({ users }) => {
     return {
-        usersList: users
+        usersList: Object.values(users)
+        .sort((a,b) => (Object.values(b.answers).length + b.questions.length)
+            - (Object.keys(a.answers).length + a.questions.length))
     }
 }
 
