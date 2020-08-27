1be2b97783d4ec3c10464db703fe4228acb45554
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-08-27 15:59:44 +0100

    feat: add loadbar to sign up

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USERS, ADD_USER } from '../constants/actionTypes'
 import { _saveNewUser } from '../utils/_DATA'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const receiveUsers = (users) => ({
     type: RECEIVE_USERS,
@@ -14,9 +15,11 @@ export const addUser = (newUser) => ({
 
 export const handleNewUser = (newUser) => {
     return (dispatch) => {
+        dispatch(showLoading())
         return _saveNewUser(newUser)
         .then((formattedUser) => {
             dispatch(addUser(formattedUser))
+            dispatch(hideLoading())
         })
     }
 }
